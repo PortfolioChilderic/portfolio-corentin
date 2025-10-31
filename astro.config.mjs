0fb07e2c9f286158ea66ff04bb2ea73cf21546c6
Childeric <corentinbozecpro@gmail.com>
2025-10-31 16:36:04 +0100

    [ADD] astro.config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,9 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://childeric.github.io',
+  base: '/portfolio/',
+  output: 'static',
+  trailingSlash: 'ignore',
+});
